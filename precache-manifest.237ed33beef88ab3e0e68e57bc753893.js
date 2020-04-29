c63c4991921e246bb15ada2390db3e99a663920e
muthuraj <muthuraj.ee18@bitsathy.ac.in>
2020-04-30 02:01:22 +0530

    Updates

---
+++ precache-manifest.237ed33beef88ab3e0e68e57bc753893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b158ee2f9a398102c09fca44a8f9bf4b",
+    "url": "/Muthuraj-web/footzone.github.io.git/index.html"
+  },
+  {
+    "revision": "9d76559d0bbfe492119c",
+    "url": "/Muthuraj-web/footzone.github.io.git/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "0cf3faaf1545e5f564b2",
+    "url": "/Muthuraj-web/footzone.github.io.git/static/css/main.d74c7114.chunk.css"
+  },
+  {
+    "revision": "9d76559d0bbfe492119c",
+    "url": "/Muthuraj-web/footzone.github.io.git/static/js/2.51f84887.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Muthuraj-web/footzone.github.io.git/static/js/2.51f84887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf3faaf1545e5f564b2",
+    "url": "/Muthuraj-web/footzone.github.io.git/static/js/main.f75ee3a0.chunk.js"
+  },
+  {
+    "revision": "68469b490e54a3ebcb0d",
+    "url": "/Muthuraj-web/footzone.github.io.git/static/js/runtime-main.f2a336ff.js"
+  }
+]);
